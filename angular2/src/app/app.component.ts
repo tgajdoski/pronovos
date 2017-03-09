336020f0a386978057bc0f9d317f1bc5f270ccff
Gajdo <t.gajdoski@gmail.com>
2017-03-09 14:55:45 +0100

    service added angular-cli

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+import { FileListService } from './services/file-list.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+   providers: [FileListService]
 })
+
 export class AppComponent {
   public uploader:FileUploader = new FileUploader({url:'http://localhost:3001/upload'});
+
+
+  public fileList: any;
+  public _id: any;
+  public originalname: any;
+  public mimetype: any;
+  public size: number;
+  public uploadDate: any;
+
+  public page = 1;
+  public fileCount: number;
+  public pageSize = 1;
+  public start = 0;
+
+ constructor(private _filelistService: FileListService) { }
+  paginateEmployee() {
+    this.start = ((this.page - 1) * this.pageSize);
+    this.loadFileList();
+  }
+
+  loadFileList() {
+    this._filelistService.getFileList()
+      .subscribe((res: any) => {
+        //console.log("Response", res)
+        this.fileList = res.data;
+        this.fileCount = res.totalCount;
+//        console.log(this.fileCount);
+      });
+  }
+
+  ngOnInit() {
+    this.loadFileList();
+  }
+
+
 }
+
+
+
+
---
+++ angular2/src/app/services/file-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, HttpModule, URLSearchParams, Headers, Response, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class FileListService {
+
+
+    private localUrl: string;
+
+    constructor(private _http: Http) {
+        this.localUrl = "http://localhost:3001";
+        console.log("Files Provider");
+    }
+
+    getFileList() {
+        return this._http.get(this.localUrl + '/files/')
+            .map(res => res.json());
+    }
+}
