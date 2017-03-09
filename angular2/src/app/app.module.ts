2e3017ad9cb5c1b074c4273d2b4c1cad0256be02
Gajdo <t.gajdoski@gmail.com>
2017-03-09 14:25:07 +0100

    promena vo angular cli - raboti update-ot

---
+++ angular2/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, FileSelectDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
