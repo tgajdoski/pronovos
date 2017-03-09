d5d602c4e22e03b83ffef2b39c55da9d54ab1f90
Gajdo <t.gajdoski@gmail.com>
2017-03-09 11:31:11 +0100

    angular perpare service for files, prepare view

---
+++ angular2-app/app/services/file-list.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, HttpModule, URLSearchParams, Headers, Response, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class FileListService {
+
+
+
+
+
+    private localUrl: string;
+
+    constructor(private _http: Http) {
+        this.localUrl = "http://192.168.10.19:3001/";
+        console.log("Files Provider");
+    }
+
+    getFileList(start: any, limit: any) {
+        return this._http.get(this.localUrl + '/files/')
+            .map(res => res.json());
+    }
+}
